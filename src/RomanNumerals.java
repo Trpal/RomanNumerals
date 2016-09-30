3a025185c67c46ce8fb971f0f1005d0c9f3417a6
jkorpeli <jkorpeli@10.30.7.28>
2016-09-30 16:59:34 +0300

    Finished exercise

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,53 @@
 
 public class RomanNumerals {
+	
+	private int arabicNumeral = 0;
+	private int lastNumber = 0;
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
 		
+		for (int i = romanNum.length() - 1; i >= 0 ; i--){
+			char convertable = romanNum.charAt(i);
+			switch(convertable){
+				case 'I':
+					arabicNumeral = handleMultiples(1, lastNumber, arabicNumeral);
+					lastNumber = 1;
+					break;
+				case 'V':
+					arabicNumeral = handleMultiples(5, lastNumber, arabicNumeral);
+					lastNumber = 5;
+					break;
+				case 'X':
+					arabicNumeral = handleMultiples(10, lastNumber, arabicNumeral);
+					lastNumber = 10;
+					break;
+				case 'L':
+					arabicNumeral = handleMultiples(50, lastNumber, arabicNumeral);
+					lastNumber = 50;
+					break;
+				case 'C': 
+					arabicNumeral = handleMultiples(100, lastNumber, arabicNumeral);
+					lastNumber = 100;
+					break;
+				case 'D':
+					arabicNumeral = handleMultiples(500, lastNumber, arabicNumeral);
+					lastNumber = 500;
+					break;
+				case 'M':
+					arabicNumeral = handleMultiples(1000, lastNumber, arabicNumeral);
+					lastNumber = 1000;
+					break;
+			}
+		}
+		
+		return arabicNumeral;
+	}
+	
+	public static int handleMultiples(int currentNum, int lastNumber, int lastDecimal ){
+		if(lastNumber > currentNum){
+			return lastDecimal - currentNum;
+		}else{
+			return lastDecimal + currentNum;
+		}
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,114 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_CreateRomanNumeral() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		assertNotNull(converter);
 	}
+	
+	@Test
+	public void test_RomanI() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("I");
+		
+		assertEquals(1, arabicInteger);
+	}
+	
+	@Test
+	public void test_RomanV() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("V");
+		
+		assertEquals(5, arabicInteger);
+	}
+	
+	@Test
+	public void test_RomanII() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("II");
+		
+		assertEquals(2, arabicInteger);
+	}
+	
+	@Test
+	public void test_RomanX() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("X");
+		
+		assertEquals(10, arabicInteger);
+		
+	}
+	
+	@Test
+	public void test_RomanL() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("L");
+		
+		assertEquals(50, arabicInteger);
+		
+	}
+
+	@Test
+	public void test_RomanC() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("C");
+		
+		assertEquals(100, arabicInteger);
+		
+	}
+
+	@Test
+	public void test_RomanD() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("D");
+		
+		assertEquals(500, arabicInteger);
+	}
+	
+	@Test
+	public void test_RomanM() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("M");
+		
+		assertEquals(1000, arabicInteger);
+		
+	}
+	
+	@Test
+	public void test_RomanIV() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("IV");
+		
+		assertEquals(4, arabicInteger);
+	}
+	
+	@Test
+	public void test_RomanIX(){
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("IX");
+		
+		assertEquals(9, arabicInteger);
+	}
+	
+	@Test
+	public void test_RomanMCMLXXXIV() {
+		RomanNumerals converter = new RomanNumerals();
+		
+		int arabicInteger = converter.convertToInteger("MCMLXXXIV");
+		
+		assertEquals(1984, arabicInteger);
+	}
+	
 
 }
